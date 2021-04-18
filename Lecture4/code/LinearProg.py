87cf3a26551802036b3ce5721098f87691089a3a
zhengwei <zhenghaku@gmail.com>
2021-04-19 01:37:04 +0800

    add linprog and monte carlo

---
+++ Lecture4/code/LinearProg.py
@@ -0,0 +1,273 @@
+# %% [markdown]
+'''
+# 线性规化问题
+
+## 什么实现新规划问题？
+
+考虑下面这个例子：
+# ![avatar](./pics/pic221.png)
+
+线性规化问题十分常见，大多数用于资源优化分配这类问题。
+'''
+# %%[markdown]
+# ![avatar](./pics/pic2.png)
+# ## 线性规划的定义和一般形式
+#
+# ### 线性规划的定义
+# 1. 每一个问题都用一组决策变量(x1, x2, ⋯, xn)表示某一方案, 这组决策变量的值就代表一个具体方案。一般这些变量取值是非负且**连续**的。
+# 2. 存在一定的**约束条件**, 这些约束条件可以用一组**线性等式**或**线性不等式**来表示
+# 3. 都有一个要求达到的**目标**, 它可用决策变量的**线性函数**(称为目标函数) 来表示。按问题的不同, 要求目标函数实现最大化或最小化。
+#
+#
+# **满足以上三个条件**的数学模型称为**线性规划**的数学模型。
+#
+#
+# ### 线性规划的一般形式
+#
+# 目标函数 $max(min) z = c_1 x_1 + c_2 x2 + \cdots + c_n x_n$
+#
+#
+# 满足约束条件
+# $$
+# \begin{cases}
+# a_{11}x_1+a_{12}x_2+\cdots+a_{1n}x_n\leq(=, \geq)b_1\\
+# a_{21}x_1+a_{22}x_2+\cdots+a_{2n}x_n\leq(=, \geq)b_2\\
+# \cdots \\
+# a_{m1}x_1+a_{m2}x_2+\cdots+a_{mn}x_n\leq(=, \geq)b_m\\
+# \end{cases}
+# $$
+# 即
+# $$
+# \begin{equation}
+# max(min) cx \\
+# s.t.: Ax=b \\
+# Ex \leq F \\
+# \underline{x} \le x \le \overline{x}
+# \end{equation}
+# $$
+
+# %% [markdown]
+'''
+# ![avatar](./pics/pic1.png)
+'''
+
+# %% [markdown]
+'''
+
+## 用Python来解线性规划问题
+
+下面我们用最简单的scipy来解线性规划问题，我们们就解上面的那个问题：
+$$
+c=-(2x_1+3X2) \\
+\begin{cases}
+x_1+2x_2\leq8\\
+41x_1\leq16\\
+41x_2\leq12\\
+x_1, x_2\geq0\\
+\end{cases}
+$$
+
+我们这里先用最简单的scipi来做
+'''
+# %%
+
+
+# 目标函数
+import pulp
+from scipy.optimize import linprog
+import numpy as np
+c = [-2, -3]
+# 约束条件的系数矩阵
+A_ub = [[1, 2], [4, 0], [0, 4], [-1, 0], [0, -1]]
+# 约束条件的上限矩阵
+b_ub = [8, 16, 12, 0, 0]
+
+res = linprog(c, A_ub, b_ub)
+print(res)
+# %%[markdown]
+'''
+## 小节一下
+
+scipy的linprog()首先优化的是一个最小化问题，我们的问题中是一个最大化的问题，
+因此我们要把目标函数乘以一个-1。
+
+所有的约束条件，也必须写成小于等于的形式，那个_ub就是上线的意思。
+
+这里还要注意一点的是scipy的决策变量默认是0-正无穷的，你可以修改这个上线。
+
+上面这个没有用到等于的约束条件，我们在加几个条件试试：
+
+$$
+c=-(2x_1+3X2) \\
+\begin{cases}
+x_1+2x_2\leq8\\
+41x_1\leq16\\
+41x_2\leq12\\
+x_1+x_2=10\\
+x_1, x_2\geq0\\
+x_1, x_2\leq10\\
+\end{cases}
+$$
+'''
+# %%
+c = [-2, -3]
+# 约束条件的系数矩阵
+A_ub = [[1, 2], [4, 0], [0, 4], [-1, 0], [0, -1]]
+# 约束条件的上限矩阵
+b_ub = [8, 16, 12, 0, 0]
+# 相等条件的系数矩阵
+a_eq = [[1, 1]]
+# 相等条件值
+b_eq = [5]
+# 决策变量的上下限
+bnd = [[0, 5], [0, 5]]
+
+res = linprog(c, A_ub, b_ub, a_eq, b_eq, bnd)
+print(res)
+# %% [markdown]
+'''
+这里解释一下linprod输出的结果：
+.con ：相等约束残差应该是接近0.
+
+.fun ：目标函数的最有值（如果成功找到）.
+
+.message :求解器返回的消息，例如出了什么问题，没有解出来.
+
+.nit ：迭代次数.
+
+.slack ：与约束条件测差距.
+
+.status ：状态0-4，0.找到最优值，
+
+.success：是否找到最优值.
+
+.x ：解出来的决策变量的值
+
+我们可以把上一题的条件改改，让他无解试试，大家看看他的message
+'''
+
+# %% [markdown]
+'''
+# quiz 1, 应用2
+![avatar](./pics/pic3.png)
+'''
+
+# %% [markdown]
+'''
+其实scipy的linprog有很多缺陷，可以说是个玩具，他主要有以下几个问题：
+1. 它不支持外部求解器（求解复杂问题时才是个问题）
+2. 它不支持整数决策变量（这是最大的问题，比如上面的例子）
+3. 仅仅支持写矩阵来建模，容易出错，其实也还好
+4. 不支持最大化问题
+5. 约束条件不支持大与等于（4，5都还好，主要是剪出来的模型描述性不高，并不影响求解）
+'''
+
+# %%
+# %% [markdown]
+# # 混合整数线性规划简介
+# ## 分支定界法
+# 分支定界法的思想主要是：把一个含整数变量规划的问题化为不含整数变量的两个子问题进行不断迭代求解。
+# ![avatar](./pics/分支界定法1.png)
+# ![avatar](./pics/分支界定法2.png)
+'''
+我们用PuLP这个包来求解一下试试，大家想用pip安装一下
+'''
+
+# %%
+# 添加4个决策变量
+V_NUM = 4
+x = [pulp.LpVariable(f"X{i}", lowBound=0, cat=pulp.LpInteger)
+     for i in range(V_NUM)]
+# 初始化一个最大化的模型
+model = pulp.LpProblem('应用2', pulp.LpMaximize)
+
+# 添加限制条件
+model += (1.5*x[0]+x[1]+2.4*x[2]+x[3] <= 2000, "equipA")
+model += (x[0]+5*x[1]+x[2]+3.5*x[3] <= 8000, "equipB")
+model += (1.5*x[0]+3*x[1]+3.5*x[2]+x[3] <= 5000, "equipC")
+# 添加目标
+model += 5.24*x[0]+7.3*x[1]+8.34*x[2]+4.18*x[3]
+# 求解模型
+status = model.solve()
+
+# 打印出结果
+for xi in model.variables():
+    print(xi.name, ' = ', xi.varValue)
+print("objective value: ", model.objective.value())
+
+# %%[markdown]
+'''
+## 小结一下
+1. PuLP解整数优化问题，只要你添加偏凉的时候指定塔的类型就行`cat=pulp.LpInteger`，
+除了整数还有binary等等类型
+2. 我们首先要建一个pulp的模型，这里可以指定他是最大还是最小化目标
+3. 我们可以用PuLP的变量写成表达式，它的类型是`pulp.pulp.LpAffineExpression`，可以添加到
+模型里面编程限制条件
+4. 如果用PuLP的变量写成表达式不是相等或者比较的，她就成了目标函数
+5. 对模型调用solve求解就可以得到你结果了，就在求解后的模型里面
+
+'''
+# %% [markdown]
+'''
+# quiz 2, 应用3
+![avatar](./pics/pic4.png)
+'''
+
+# %% [markdown]
+'''
+# quiz 3, 应用4
+![avatar](./pics/pic5.png)
+
+决策变量或者约束条件很多的时侯，建议还是把系数写成矩阵。
+那用pulp怎么做呢？其实很简单，你有了矩阵就可以用循环求和来吧所有的约束条件写出来。
+'''
+# %% [markdown]
+'''
+## 二进制规化
+我们把上面这个问题在高的复杂一点：假如，方案3，4只能采用其中一个，怎么求解？
+
+我们这个时候引入两个二进制变量就是0，1的变量y2，y3，这个代表哪种方案能采用。
+两种不能同时采用那么就是y2+y3<=1.
+
+当y2=0的时候不能采用方案3，所以可以写成x3<=y2*100，x3肯定不会大与100，
+所以可以这么写。
+'''
+
+# %%
+model = pulp.LpProblem('应用4', pulp.LpMinimize)
+V_NUM = 5
+x = [pulp.LpVariable(
+    f'x{i}', lowBound=0, cat=pulp.LpInteger) for i in range(V_NUM)]
+
+y = [pulp.LpVariable(
+    f'y{i}', lowBound=0, cat=pulp.LpBinary) for i in (2, 3)]
+# 这里可以把约束条件和目标系数写成矩阵的形式，然后用求和来写表达式
+c = [0, 0.1, 0.2, 0.3, 0.8]
+objective = sum(c[i] * x[i] for i in range(V_NUM))
+
+constraints = []
+a_cons = ([1, 2, 0, 1, 0], [0, 0, 2, 2, 1], [3, 1, 2, 0, 3])
+b_cons = 100
+# enumerate，除了没每一个元素还给出它的index
+for idx, a in enumerate(a_cons):
+    constraints.append(sum([a[i] * x[i] for i in range(V_NUM)]) == b_cons)
+# 添加目标个条件
+model += objective
+for cons in constraints:
+    model += cons
+
+# 添加二进制的条件
+model += (y[0]+y[1] <= 1, "not same time")
+model += (x[2] <= 100*y[0], "chose x2")
+model += (x[3] <= 100*y[1], "chose x3")
+
+# 求解模型
+status = model.solve()
+
+# 打印出结果
+for xi in model.variables():
+    print(xi.name, ' = ', xi.varValue)
+print("objective value: ", model.objective.value())
+
+
+# ref https://realpython.com/linear-programming-python/
---
+++ Lecture4/code/LinearProg2.py
@@ -0,0 +1,184 @@
+# %% [markdown]
+# # 电力系统经济运行问题的数学建模与仿真与非线性优化
+# ![avatar](./pics/pic6.png)
+# ![avatar](./pics/pic7.png)
+
+# %% [markdown]
+# # 电力系统最优潮流的定义
+# ## 定义
+# 当系统的结构参数和负荷情况都已**给定**时，调节可利用的**控制变量**（如发电机输出功率、可调变压器抽头等）来找到能满足所有
+# **运行约束条件**的，并使系统的某一**性能指标**（如发电成本或网络损耗）达到**最优值**下的潮流分布
+# ![avatar](./pics/pic8.png)
+# **控制变量**：发电机输出功率、可调变压器抽头等
+#
+# **运行约束**：功率平衡、发电机出力、线路容量、节点电压等
+#
+# **性能指标**：发电成本或网络损耗等
+
+# %% [markdown]
+# # 电力系统最优潮流数学模型
+# ## 基于交流潮流的电力系统最优潮流模型
+# ### 目标函数
+# $$ min f(x)=\sigma_{i \in S_G} [a_iP_{Gi}^2+b_iP_{Gi}+c_i] $$
+# ### 运行约束
+# 1. 节点功率平衡方程
+# $$ V_i\sum_{j=1}^nV_j(G_{ij}cos\theta_{ij} + B_{ij}sin\theta_{ij})-P_{Gi}+P_{Di}=0\quad \forall i=1, \cdots, N $$
+# $$ V_i\sum_{j=1}^nV_j(G_{ij}sin\theta_{ij}-B_{ij}cos\theta_{ij})-Q_{Gi}+Q_{Di}=0 \quad \forall i = 1, \cdots, N $$
+# 2. 各变量上下限
+# $$ \underline{P}_{Gi} \le P_{Gi} \le \overline{P}_{Gi} \quad \underline{Q}_{Gi} \le Q_{Gi} \le \overline{Q}_{Gi} \quad \underline{V}_{i} \le V_{i} \le \overline{V}_{i} $$
+# $$ \underline{P}_{ij} \le P_{ij} = V_iV_j(G_{ij}cos\theta_{ij} + B_{ij}sin\theta_{ij}) - V_i^2G_{ij} \le \overline{P}_{ij} $$
+# %% [markdown]
+# ### 目标函数
+# $$ min f(x)=\sigma_{i \in S_G} [a_iP_{Gi}^2+b_iP_{Gi}+c_i] $$
+# ### 运行约束
+# 1. 节点功率平衡方程
+# $$ \sum_{j=1}^n\frac{\theta_{ij}}{X_{ij}} - P_{Gi}+P_{Di} = 0 \quad \Rightarrow \quad P_N=Y_\theta \quad \Rightarrow \quad \sum_{i\in S_G}P_{Gi}=D $$
+# 2. 各变量上下限
+# $$ \underline{P}_{Gi} \le P_{Gi} \le \overline{P}_{Gi} \quad \Rightarrow \quad \underline{P}_{G} \le P_{G} \le \overline{P}_{G} $$
+# $$ \underline{P}_{ij} \le \frac{\theta_{ij}}{X_{ij}} \le \overline{P}_{ij} \quad \Rightarrow \quad \underline{P}_{L} \le SP_{N} \le \overline{P}_{L} $$
+# %% [markdown]
+# ### 目标函数
+# $$ min \sum_{k\in K}\sum_{j\in J}[c^p(j, k) + c^u(j, k) + c^d(j, k)] $$
+# ### 功率平衡约束
+# $$ \sum_{j\in J}p(j, k) = D(k) $$
+# ### 机组最大最小出力约束
+# $$ \underline{p}(j, k) \le p(j, k) \le \overline{p}(j, k) $$
+# $$ \underline{P}_jv(j, k) \le \underline{p}(j, k) $$
+# $$ \overline{p}(j, k) \le \overline{P}_jv(j, k) $$
+# ### 系统正、负旋转备用约束
+# $$ \sum_{j\in J}\overline{p}(j, k) \ge D(k) \cdot (1+L\%) $$
+# $$ \sum_{j\in J}\underline{p}(j, k) \le D(k) $$
+# ### 线路容量约束
+# $$ -f_{lim} \le Sp \le f_{lim} $$
+# ### 常规机组爬坡容量约束
+# $$
+# \begin{aligned}
+# \overline{p}(j, k)
+# &\le p(j, k - 1) + RU_jv(j, k - 1) \\
+# &\le SU_j[v(J, k)-v(j, k - 1)] \\
+# &\le \overline{P}_j[1-v(j, k)]
+# \end{aligned}
+# $$
+# $$ \overline{p}(j, k) \le \overline{P}_jv(j, k + 1) + SD_j[v(j, k) - v(j, k + 1)] $$
+# $$
+# \begin{aligned}
+# p(j, k - 1) - \underline{p}(j, k)
+# &\le RD_jv(j, k) \\
+# &+ SD_j[v(j, k - 1) - v(j, k)] \\
+# &+ \overline{P}_j[1-v(j, k - 1)]
+# \end{aligned}
+# $$
+# ### 最小启停时间约束
+# $$ \sum_{k=1}^{G_i}[1-v(j, k)] = 0 $$
+# $$ \sum_{n=k}^{k+UT_j-1}v(j, n) \ge UT_j[v(j, k)-v(j, k - 1)] $$
+# $$ \sum_{n=k}^{T} \{v(j, n)- [v(j, k) - v(j, k - 1)] \} \ge 0$$
+# $$ \sum_{k=1}^{L_i}v(j, k) = 0 $$
+# $$ \sum_{n=k}^{k+DT_j-1} [1 - v(j, n)] \ge DT_j[v(j, k - 1) - v(j, k)] $$
+# $$ \sum_{n=k}^{T} \{ 1-v(j, n) - [v(j, k - 1) - v(j, k)] \} \ge 0 $$
+# %%
+# %% [markdown]
+# ## 如何建模和求解下面这个问题？
+# <div align=center><img src='./pics/pic10.png' /></div>
+#
+# **数学模型：**
+# $$ min: \{ F=F_1(P_{G1})+F_2(P_{G2}) \} $$
+# $$ st.: P_{G1}+P_{G2}=P_{LD} $$
+# **参数：**
+# $$ F_1=0.0007P_{G1}^2+0.30P_{G1}+4 $$
+# $$ F_2=0.0004P_{G2}^2+0.32P_{G2}+3 $$
+# $$ P_{LD2}=100MW $$
+# **求解：**
+# $$
+# min:
+# \begin{cases}
+# F &= 0.0007P_{G1}^2+0.30P_{G1}+4 \\
+#   &+ 0.0004P_{G2}^2+0.32P_{G2}+3
+# \end{cases}
+# $$
+# $$ st.: P_{G1}+P_{G2}=100 $$
+
+# %%[markdown]
+
+# <div align=center><img src='./pics/pic11.png' /></div>
+# ## 将这个问题线性化
+# 上面的问题是一个非线性问题。由于机组出力有最大最小的限制，连接二次曲线首末两端可以对目标函数进行线性化处理得到下式：
+# $$
+# min:
+# \begin{cases}
+# F &= 0.72P_{G1}-52 \\
+#   &+0.44P_{G2}-5
+# \end{cases}
+# $$
+# $$
+# \begin{aligned}
+# st.:  & P_{G1}+P_{G2}=100 \\
+# & 30 \le P_{G1} \le 150 \\
+# & 0 \le P_{G2} \le 50
+# \end{aligned}
+# $$
+
+# %% [markdown]
+
+# ## Quiz 3 课堂练习
+
+# 请把下面的问题线性化以后求解：
+
+#![](./pics/pic14.png)
+# $$
+# S=\left[
+# \begin{matrix}
+# 0 & -7/12 & -1/4 \\
+# 0 & -5/12 & -3/4 \\
+# 0 & 5/12 & -1/4
+# \end{matrix}
+# \right]
+# $$
+#
+# 目标函数
+# $$
+# min:
+# \begin{cases}
+# F &= 0.0007P_{G1}^2+0.30P_{G1}+4 \\
+#   &+ 0.0004P_{G2}^2+0.32P_{G2}+3
+# \end{cases}
+# $$
+# 运行约束：
+# 1. 节点功率平衡方程
+# $$ P_{G1} + P_{G2} = 100 $$
+# 2. 发电机出力上下限
+# $$ 30 \le P_{G1} \le 150 $$
+# $$ 0 \le P_{G2} \le 50 $$
+# 3. 线路潮流上下限
+# $$
+# -\left[
+# \begin{matrix}
+# 30 \\
+# 80 \\
+# 50 \\
+# \end{matrix}
+# \right]
+# \le S
+# \left[
+# \begin{matrix}
+# P_{G1} \\
+# P_{G2} \\
+# -100 \\
+# \end{matrix}
+# \right]
+# \le
+# \left[
+# \begin{matrix}
+# 30 \\
+# 80 \\
+# 50 \\
+# \end{matrix}
+# \right]
+# $$
+# %% [markdown]
+'''
+## 非线性规化
+
+对于上面的问题我们也可以坐非线性规划求解，
+@甘睿，这里用SciPy的optimize.minimize操作一下
+
+'''
